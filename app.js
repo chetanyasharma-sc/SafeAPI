0d54bb6f2a435caca8b5ee268cb6554e81e88fb5
chetanyasharma <chetanyasharma97@gmail.com>
2025-03-30 20:58:55 -0400

    another commit

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 function validate(event){
 
-    event.preventDefault();
-    let username =  document.getElementById('username').value;
-    let password = document.getElementById('password').value;
 
-    if (username == "" || password == "" ){
-        alert("Please fill in all fields");
+    var username =  document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username === "" || password === "" ){
+            alert("Please fill in all fields");
     
         }   
         if (username === "admin" && password === "1234"){
-            alert("Login successful ")
+            alert("sucessful");
+           
 
            
             } 
